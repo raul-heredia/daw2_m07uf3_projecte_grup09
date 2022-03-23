987d6f98d6df5efcd531dabc80b17f54a7b19361
Raúl Heredia <raul.heredia01@outlook.com>
2022-03-23 19:06:47 +0100

    Co-authored-by: anmefe2122daw2 <anmefe2122daw2@users.noreply.github.com>

--- flyfly/resources/views/usuaris/login.blade.php
+++ flyfly/resources/views/usuaris/login.blade.php
@@ -24,40 +24,113 @@
             margin-left: 5px;
         }
 
+        body.login {
+            background: linear-gradient(90deg, #4b6cb7 0%, #182848 100%);
+        }
+
+        .login,
+        .formldap {
+            width: 360px;
+            padding: 8% 0 0;
+            margin: auto;
+            font-family: 'Nunito', cursive;
+        }
+
+        .form {
+            position: relative;
+            z-index: 1;
+            background: #FFFFFF;
+            border-radius: 10px;
+            max-width: 360px;
+            margin: 0 auto 100px;
+            padding: 45px;
+            text-align: center;
+        }
+
+        .form input {
+            outline: 0;
+            background: #f2f2f2;
+            width: 100%;
+            border: 0;
+            border-radius: 5px;
+            margin: 0 0 15px;
+            padding: 15px;
+            box-sizing: border-box;
+            font-size: 14px;
+            font-family: 'Nunito', cursive;
+        }
+
+        .form input:focus {
+            background: #dbdbdb;
+        }
+
+        .form .button {
+            font-family: 'Nunito', cursive;
+            text-transform: uppercase;
+            outline: 0;
+            background: #4b6cb7;
+            width: 100%;
+            border: 0;
+            border-radius: 5px;
+            padding: 15px;
+            color: #FFFFFF;
+            font-size: 14px;
+            -webkit-transition: all 0.3 ease;
+            transition: all 0.3 ease;
+            cursor: pointer;
+        }
+
+        .form .button:active {
+            background: #395591;
+        }
+
+        h5 {
+            margin-bottom: 15px;
+        }
+
+        a.nav-link {
+            color: white !important;
+        }
+
+        .hidden {
+            display: none;
+        }
+
+        .inline {
+            display: inline-block;
+        }
+
+        .flex-form {
+            gap: 5px !important;
+        }
+
     </style>
 </head>
 
-<body>
-    <div class="container">
-        <h1>Accés a FlyFly</h1>
-        <div class="card mt-5">
-            <div class="card-header">
-                Validació de l'usuari
-            </div>
-            <div class="card-body">
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-                <form method="get" action="{{ route('autenticacio') }}">
+<body class="login">
+    <div class="login">
+        <div class="card-body">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <div class="form">
+                <form class="login-form" method="get" action="{{ route('autenticacio') }}" autocomplete="off">
                     @csrf
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="text" class="form-control" name="email" />
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" name="password" />
-                    </div>
-                    <button type="submit" class="btn btn-block btn-primary">Iniciar Sessió</button>
+                    <h5>Accés Administració FlyFly</h5>
+                    <input type="email" name="email" placeholder="Usuari" required />
+                    <input type="password" name="password" placeholder="Contrasenya" required />
+                    <input type="submit" class="button" value="Iniciar Sessió" />
                 </form>
             </div>
         </div>
+    </div>
+    </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
